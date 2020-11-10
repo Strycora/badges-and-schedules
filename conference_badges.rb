4f745be7d5625d085d9d4d20ecc5dc80f141ccc7
Vikram Chatterjee <strycora@gmail.com>
2020-11-10 12:24:04 -0700

    commit tree 6deb8d643f870cea9aa2ce5bc81413266e3550e1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    badge_blurbs = []
+    array.each do |name|
+        badge_blurbs << "Hello, my name is #{name}."
+    end
+    return badge_blurbs
+end
+
+def assign_rooms(array)
+    welcome_messages = []
+    array.each_with_index do |name, index|
+       welcome_messages << "Hello, #{name}! You'll be assigned to room #{(index+1)}!"
+    end
+return welcome_messages
+end
+
+def printer(attendees)
+    badges = batch_badge_creator(attendees)
+    badges.each do |name|
+        puts name
+    end
+    rooms = assign_rooms(attendees)
+    rooms.each do |assign|
+        puts assign
+    end
+
+end
